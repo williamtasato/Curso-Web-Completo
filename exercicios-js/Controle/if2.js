f9ea4036414dffb59129b047588dd3f93e05e766
William_ciandt <william@cisistemas.com.br>
2020-02-28 14:31:39 -0300

    if

---
+++ exercicios-js/Controle/if2.js
@@ -0,0 +1,18 @@
+function teste(num){
+    if(num > 7)
+    console.log(num)
+
+    console.log('Final')    
+}
+
+teste(6)
+teste(8)
+
+function teste2(num){
+    if(num > 7);{// cuidado com o ; não usar com as estruturas de controle
+        console.log(num)
+    }
+}
+
+teste2(6)
+teste2(8)
