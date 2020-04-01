ecfc856808d8f26f369ebcb24ef53d783f8d74e6
William Tasato <wg-tasato@bol.com.br>
2020-03-31 21:14:08 -0300

    Finish Vue Project

--- exercicios-web/react/crud/frontend/src/main/App.jsx
+++ exercicios-web/react/crud/frontend/src/main/App.jsx
@@ -2,16 +2,19 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Home from '../components/home/Home'
 import Footer from '../components/template/Footer'
+import Routes from './Routes'
 
 export default props =>
+<BrowserRouter>
 <div className="app">
     <Logo />
     <Nav />
-    <Home />
+    <Routes />
     <Footer />
 </div>
+</BrowserRouter>
