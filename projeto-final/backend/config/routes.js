ccaed26fa0cd8611a53c46639f3564f01eb785e0
William Tasato <wg-tasato@bol.com.br>
2020-04-05 22:48:50 -0300

    backend start

--- projeto-final/backend/config/routes.js
+++ projeto-final/backend/config/routes.js
@@ -1,4 +1,40 @@
 module.exports = app => {
+    app.post('/signup',app.api.user.save)
+    app.post('/signin',app.api.auth.signin)
+    app.post('/validateToken',app.api.auth.validateToken)
+
+
     app.route('/users')
         .post(app.api.user.save)
+        .get(app.api.user.get)
+
+    app.route('/users/:id')
+        .put(app.api.user.save)
+        .get(app.api.user.getById)
+
+    app.route('/categories')
+        .get(app.api.category.get)
+        .post(app.api.category.save)   
+
+// Cuidado com a ordem! Tem que vir antes de '/categories/:id'
+    app.route('/categories/tree')
+    .get(app.api.category.getTree)
+
+        app.route('/categories/:id')
+        .get(app.api.category.getById)
+        .put(app.api.category.save)
+        .delete(app.api.category.remove)
+
+        app.route('/articles')
+        .get(app.api.article.get)
+        .post(app.api.article.save)
+        
+
+    app.route('/articles/:id')
+        .get(app.api.article.getById)
+        .put(app.api.article.save)   
+        .delete(app.api.article.remove)
+
+    app.route('/categories/:id/articles')
+        .get(app.api.article.getByCategory)
 }
--- projeto-final/backend/knexfile.js
+++ projeto-final/backend/knexfile.js
@@ -1,20 +1,11 @@
 // Update with your config settings.
 
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: 'knowledge',
+      user:     'postgres',
+      password: '123456'
     },
     pool: {
       min: 2,
@@ -23,22 +14,5 @@ module.exports = {
     migrations: {
       tableName: 'knex_migrations'
     }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
 
 };
